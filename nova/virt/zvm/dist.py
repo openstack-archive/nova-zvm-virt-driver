54a31ec9d7d8c583062630ea7544439e03abd294
jichenjc <jichenjc@cn.ibm.com>
2015-04-13 01:49:50 +0800

    Fix SLES initialization network issue
    SLES also need to restart its udev to make udev to read
    configuration, so add znetconf -R and -A command to let
    kernel rescan all devices and readd them.
    
    Change-Id: I9be4430bddfcdc298bc0c013e4feaccc5e1ce106

--- nova/virt/zvm/dist.py
+++ nova/virt/zvm/dist.py
@@ -295,9 +295,11 @@ class sles(LinuxDist):
 class sles11(sles):
     def get_znetconfig_contents(self):
         return '\n'.join(('cio_ignore -R',
+                          'znetconf -R -n',
                           'udevadm trigger',
                           'udevadm settle',
                           'sleep 2',
+                          'znetconf -A',
                           'service network restart',
                           'cio_ignore -u'))
 
@@ -308,9 +310,7 @@ class sles11(sles):
 class sles12(sles):
     def get_znetconfig_contents(self):
         return '\n'.join(('cio_ignore -R',
-                          'znetconf -R <<EOF',
-                          'y',
-                          'EOF',
+                          'znetconf -R -n',
                           'udevadm trigger',
                           'udevadm settle',
                           'sleep 2',
