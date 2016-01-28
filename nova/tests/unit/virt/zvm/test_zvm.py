500bab5f431217cc023b085479b2f54a369ce793
Yi Chun Huang <hychuang@cn.ibm.com>
2016-01-28 14:45:38 +0800

    Restrict acceptable states of the instance when attaching volumes
    Raise an error if the state of the instance is not ACTIVE or
    SHUTDOWN when attaching a volume to it.
    
    Closes-Bug: 1539490
    
    Change-Id: Id9d784d783b572a8be879ae7ec46bca3611a015d

--- nova/tests/unit/virt/zvm/test_zvm.py
+++ nova/tests/unit/virt/zvm/test_zvm.py
@@ -28,6 +28,7 @@ from oslo_serialization import jsonutils
 from oslo_utils import fileutils
 
 from nova.compute import power_state
+from nova.compute import vm_states
 from nova import context
 from nova import db
 from nova import exception as nova_exception
@@ -110,7 +111,8 @@ class ZVMTestCase(test.TestCase):
                                   'root_gb': 10,
                                   'ephemeral_gb': 0,
                                   'image_ref': '0000-1111',
-                                  'host': 'fakenode'})
+                                  'host': 'fakenode',
+                                  'vm_state': vm_states.ACTIVE})
         self.instance2 = db.instance_create(self.context,
                                  {'user_id': 'fake',
                                   'project_id': 'fake',
@@ -924,6 +926,20 @@ class ZVMDriverTestCases(ZVMTestCase):
         self.driver.attach_volume({}, self._fake_connection_info(),
                                   self.instance, '/dev/sdd')
         self.mox.VerifyAll()
+
+    def test_attach_volume_invalid_vm_states(self):
+        fake_instance = {'vm_state': vm_states.PAUSED}
+        self.assertRaises(exception.ZVMDriverError,
+                          self.driver.attach_volume,
+                          {}, None, fake_instance, None)
+        fake_instance = {'vm_state': vm_states.RESIZED}
+        self.assertRaises(exception.ZVMDriverError,
+                          self.driver.attach_volume,
+                          {}, None, fake_instance, None)
+        fake_instance = {'vm_state': vm_states.SOFT_DELETED}
+        self.assertRaises(exception.ZVMDriverError,
+                          self.driver.attach_volume,
+                          {}, None, fake_instance, None)
 
     def test_attach_volume_no_mountpoint(self):
         self.mox.StubOutWithMock(self.driver, 'instance_exists')
