9c66f6d6c0bd0a9153cd9ce9022253434669daee
jichenjc <jichenjc@cn.ibm.com>
2016-05-02 15:04:21 +0800

    Add conf for zvm_xcat_master
    Add conf for zvm_xcat_master
    
    part of blueprint: conf-enhancement
    
    Change-Id: I328e7b20b7f1a69980e06f50f3c89469924703ed

--- nova/virt/zvm/conf.py
+++ nova/virt/zvm/conf.py
@@ -52,6 +52,19 @@ the REST API call is made.
 Related:
     zvm_xcat_username
 """),
+    cfg.StrOpt('zvm_xcat_master',
+               default=None,
+               help="""
+The xCAT management node name from the xCAT database.
+
+The nova zvm driver sometimes needs to interact with the xCAT management node
+itself, for example to query the free disk space on the node.
+ In that case it needs this node name for the REST API call.
+
+Possible values:
+    A string specifying the name of the xCAT management node.
+    This name should come from the xCAT node database.
+"""),
     cfg.StrOpt('zvm_diskpool',
                default=None,
                help="""
@@ -260,9 +273,6 @@ zvm_image_opts = [
                help='The threshold for xCAT free space, if snapshot or spawn '
                      'check xCAT free space not enough for its image '
                      'operations, it will prune image to meet the threshold'),
-    cfg.StrOpt('zvm_xcat_master',
-               default=None,
-               help='The xCAT MM node name'),
     cfg.StrOpt('zvm_image_compression_level',
                default=None,
                help='The level of gzip compression used when capturing disk'),
