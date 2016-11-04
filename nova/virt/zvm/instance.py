434c1007924c98cabe316ebc7fceccbf37600e93
jichenjc <jichenjc@cn.ibm.com>
2016-11-04 09:29:24 +0800

    need raise exception when power on command fail
    we should let driver raise exception if power on command failed,
    otherwise, we will wait for instance to be reachable which
    will cost additional time and hide the root cause.
    
    Change-Id: I259b589f1223766863eac5346fc7c7d30ecefd8c

--- nova/virt/zvm/instance.py
+++ nova/virt/zvm/instance.py
@@ -90,6 +90,7 @@ class ZVMInstance(object):
                 # Instance already not active
                 LOG.warn(_LW("z/VM instance %s already active") % self._name)
                 return
+            raise nova_exception.InstancePowerOnFailure(reason=err_str)
 
         self._wait_for_reachable()
         if not self._reachable:
