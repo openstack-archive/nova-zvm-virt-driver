ca3ea53be6de25d0a7c97eec45fc9ea2c1e7d9bb
Huang Rui <bjhuangr@cn.ibm.com>
2018-06-21 14:24:04 +0800

    backport zvm driver from master to stable/pike
    Change-Id: I5719cc77f39f5b85698a1ae07b3435a6c9ccd1f9

--- nova_zvm/virt/zvm/exception.py
+++ nova_zvm/virt/zvm/exception.py
@@ -1,4 +1,4 @@
-# Copyright 2013 IBM Corp.
+# Copyright 2018 IBM Corp.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
@@ -17,66 +17,22 @@ from nova import exception
 from nova.i18n import _
 
 
-class ZVMBaseException(exception.NovaException):
-    """Base z/VM exception."""
-    pass
+class ZVMDriverException(exception.NovaException):
+    msg_fmt = _("ZVM Driver has error: %(error)s")
 
 
-class ZVMDriverError(ZVMBaseException):
-    msg_fmt = _('z/VM driver error: %(msg)s')
+class ZVMConnectorError(ZVMDriverException):
+    msg_fmt = _("zVM Cloud Connector request failed: %(results)s")
 
+    def __init__(self, message=None, **kwargs):
+        """Exception for zVM ConnectorClient calls.
 
-class ZVMXCATRequestFailed(ZVMBaseException):
-    msg_fmt = _('Request to xCAT server %(xcatserver)s failed: %(msg)s')
+        :param results: The object returned from ZVMConnector.send_request.
+        """
+        super(ZVMConnectorError, self).__init__(message=message, **kwargs)
 
-
-class ZVMInvalidXCATResponseDataError(ZVMBaseException):
-    msg_fmt = _('Invalid data returned from xCAT: %(msg)s')
-
-
-class ZVMXCATInternalError(ZVMBaseException):
-    msg_fmt = _('Error returned from xCAT: %(msg)s')
-
-
-class ZVMVolumeError(ZVMBaseException):
-    msg_fmt = _('Volume error: %(msg)s')
-
-
-class ZVMImageError(ZVMBaseException):
-    msg_fmt = _("Image error: %(msg)s")
-
-
-class ZVMGetImageFromXCATFailed(ZVMBaseException):
-    msg_fmt = _('Get image from xCAT failed: %(msg)s')
-
-
-class ZVMNetworkError(ZVMBaseException):
-    msg_fmt = _("z/VM network error: %(msg)s")
-
-
-class ZVMXCATXdshFailed(ZVMBaseException):
-    msg_fmt = _('Execute xCAT xdsh command failed: %(msg)s')
-
-
-class ZVMXCATCreateNodeFailed(ZVMBaseException):
-    msg_fmt = _('Create xCAT node %(node)s failed: %(msg)s')
-
-
-class ZVMXCATCreateUserIdFailed(ZVMBaseException):
-    msg_fmt = _('Create xCAT user id %(instance)s failed: %(msg)s')
-
-
-class ZVMXCATUpdateNodeFailed(ZVMBaseException):
-    msg_fmt = _('Update node %(node)s info failed: %(msg)s')
-
-
-class ZVMXCATDeployNodeFailed(ZVMBaseException):
-    msg_fmt = _('Deploy image on node %(node)s failed: %(msg)s')
-
-
-class ZVMConfigDriveError(ZVMBaseException):
-    msg_fmt = _('Create configure drive failed: %(msg)s')
-
-
-class ZVMRetryException(ZVMBaseException):
-    pass
+        results = kwargs.get('results', {})
+        self.overallRC = results.get('overallRC')
+        self.rc = results.get('rc')
+        self.rs = results.get('rs')
+        self.errmsg = results.get('errmsg')
